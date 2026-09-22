e3db373dcd2bc542c8ab711ca483902db410950f
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2026-09-22 16:58:21 +0300

    feat: general fastapi app & confic setup, env

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title=settings.app_name,
+        debug=settings.debug,
+    )
+    return app
+
+app = create_app()
